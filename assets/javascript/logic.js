1e5d709391b1d3c9abbbc084fcb360170a7c8ab2
Paul Davis <pauldavis@Pauls-MacBook-Pro.local>
2019-04-16 14:08:53 -0400

    work on search bar

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,36 +1,59 @@
 $(document).ready(function() { 
 
-    var buttonsArr = []
+    var buttonsArr = [],
         buttonDiv = $('#button-div')
-        button = $('#button')
+        // button = $('#button') // not sure if necessary
 
 
-    $.ajax({ // ajax call for defaiult buttons
-        url: 'https://api.giphy.com/v1/gifs/trending?api_key=d0oobYxRk095EpbUG3rFBW8diHv0ioio&limit=15&rating=G', // static URL - pulls first 15 trending pictures
+    $.ajax({ // ajax call for default buttons
+        url: 'https://api.giphy.com/v1/gifs/trending?api_key=d0oobYxRk095EpbUG3rFBW8diHv0ioio&limit=10&rating=G', // static URL - pulls first 15 trending pictures
         method: "GET"
         }).then(function(response) {
-            for (var i=0; i<15; i++) {
-                var tempTitle = response.data[i].title // get the title for each picture
+            for (var i=0; i<10; i++) {
+                var title = response.data[i].title // get the title for each picture
+                buttonCreate(title)
                 
-                if (tempTitle.length != "" ) { // if it has a title
-                    console.log(tempTitle)
-                tempTitle = tempTitle.replace(/ GIF.*$/i, ""); // cut off everything after "GIF" including it in the title
-                buttonsArr.push(tempTitle); // add to buttonsArr
-                // tempTitleSplit = tempTitle.split(" ",2) // to add data-name shortened to the first word
-                $('<button>')
-                    .text( tempTitle )
-                    // .attr('data-name', tempTitleSplit[0])
-                    .attr('data-nameFull', tempTitle)
-                    .attr('id', 'button')
-                    .attr('class', 'btn btn-primary btn-small btn-block')
-                    .attr('type', 'button')
-                    .appendTo( $(buttonDiv) );
+            }
+        })
+        
 
-                }
-           }
-           console.log(buttonsArr)
+    function buttonCreate(string) {
+        if (string.length != "" ) { // if it has a title
+        titleDisplay = string.replace(/ GIF.*$/i, ""); // cut off everything after "GIF" including it in the title
+        buttonsArr.push(titleDisplay); // add to buttonsArr
+        // tempTitleSplit = tempTitle.split(" ",2) // to add data-name shortened to the first word
+        console.log(titleDisplay)
+        $('<button>')
+            .text( titleDisplay )
+            // .attr('data-name', tempTitleSplit[0])
+            .attr('type', 'submit')
+            .attr('data-nameFull', titleDisplay)
+            .attr('id', 'button')
+            .attr('class', 'btn btn-primary btn-sm btn-block')
+            .appendTo( $(buttonDiv) );
+        }
+    }
+
+
+    $('#button-div').on('click', 'button', function() {
+        // console.log($(this).attr('data-nameFull'))
+        var toSearch = $(this).attr('data-nameFull'),
+            queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=d0oobYxRk095EpbUG3rFBW8diHv0ioio&q=' + toSearch +'&limit=10&offset=0&rating=G&lang=en'
+        
+        $.ajax({ // ajax call for defaiult buttons
+            url: queryURL,
+            method: "GET"
+            }).then(function(response) {
+                console.log(response)
+            })
     })
 
+    $('#search-div').on('click', 'button', function() {
+        var userSearch = $('#userSearch')
+        var search = userSearch.val()
 
+        console.log(search)
+        userSearch.val("") // clear text box
+    })
 
 })
