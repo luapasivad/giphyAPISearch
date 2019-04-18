4e37bbf985b2fe87a08e883a7f749c6dde4033cf
Paul Davis <pauldavis@Pauls-MacBook-Pro.local>
2019-04-18 12:59:47 -0400

    minimally functional - cleaned and commented

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,21 +1,23 @@
 $(document).ready(function() { 
 
     var buttonsArr = [],
-        buttonDiv = $('#button-div')
+        buttonDiv = $('#button-div'),
         gifDiv = $('#gif-div')
+
         // button = $('#button') // not sure if necessary
 
 
     $.ajax({ // ajax call for default buttons
-        url: 'https://api.giphy.com/v1/gifs/trending?api_key=iUxtIFwExyAwP5SO88cIsiR0vlJqdLvX&limit=10&rating=G', // static URL - pulls first 15 trending pictures
+        url: 'https://api.giphy.com/v1/gifs/trending?api_key=iUxtIFwExyAwP5SO88cIsiR0vlJqdLvX&limit=7&rating=G', // static URL - pulls first 15 trending pictures
         method: "GET"
         }).then(function(starterButtons) {
             console.log(starterButtons) // for test
-            for (var i=0; i<10; i++) {
+            for (var i=0; i<7; i++) {
                 var title = starterButtons.data[i].title // get the title for each picture
                 buttonCreate(title)
             }
-        })       
+        })
+       
 
 
     function buttonCreate(string) {
@@ -25,8 +27,7 @@ $(document).ready(function() {
         // tempTitleSplit = tempTitle.split(" ",2) // to add data-name shortened to the first word
         console.log(titleDisplay)
         $('<button>')
-            .text( titleDisplay )
-            // .attr('data-name', tempTitleSplit[0])
+            .text( titleDisplay ) // .attr('data-name', tempTitleSplit[0])
             .attr('type', 'submit')
             .attr('data-nameFull', titleDisplay)
             .attr('id', 'button')
@@ -36,18 +37,28 @@ $(document).ready(function() {
     }
 
     function findGif(string) {
+
         var toSearch = string // sets the dynamic api link
         var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=iUxtIFwExyAwP5SO88cIsiR0vlJqdLvX&q=' + toSearch +'&limit=10&offset=0&rating=G&lang=en'
-            
+        var gifTarget;
+        var tutorial = false; // has the tutorial run
+        
+        if (tutorial == false) {
+            tutorial = true // no more tutorial
+            $('.example').remove() // clear out tutorial
+        }
+
         $.ajax({ // ajax call for user made buttons
             url: queryURL,
             method: "GET"
             }).then(function(response) {
+
                 for (var i=0; i<10; i++) { // takes all 10 results
                     console.log(response)
                     var gifPicture = response.data[i].images.downsized.url // moving URL
                     var stillPicture = response.data[i].images.downsized_still.url // still URL
                     var gifName = response.data[i].title.replace(/ GIF.*$/i, ""); // gif title with end cut off
+                        gifTarget = response.data[i].id
                     var userGif;
 
                     if (response.data[i].username != "") { // needs work
@@ -55,35 +66,44 @@ $(document).ready(function() {
                     } else {
                         userGif = ""
                     }
-    
-
 
                     $('<div>') // container holding each gif and its info
-                        .attr('id', 'gifContainer') // targeting
-                        .attr('class', 'gif') // styling
-                        .prependTo( $(gifDiv) )
-
-                    $('<div>') // text placed within container
-                        .attr('id', 'gifText') // targeting & styling
-                        .text(gifName + userGif) // title - user
-                        .prependTo( $('#gifContainer') ) // 
+                        .attr('class', 'gifContainer gif') // targeting
+                        .attr('id', gifTarget)
+                        .appendTo( $(gifDiv) )
 
                     $('<img>') // prepends them to the screen
                         .attr('src', gifPicture) // picture viewed
-                        .attr('class', 'img-fluid') // responsive img
+                        .attr('class', 'img-fluid gif') // responsive img
                         .attr('data-animate', gifPicture) // data animate URL
                         .attr('data-still', stillPicture) // data still URL
                         .attr('data-state', 'animate') // current state
-                        .attr('id', 'gif') // styiling
-                        .prependTo( $('#gifContainer') )
-                
+                        .prependTo( $('#' + gifTarget) )
+
+                    $('<div>') // text placed within container
+                        .attr('class', 'gifText') // targeting & styling
+                        .html(gifName + userGif) // title - user
+                        .prependTo( $('#' + gifTarget) ) // 
+                    
                 }
             })
+                    //button to add more gifs, didnt work as intended and not
+                    //necessarry but saving in case I want to add a button at the
+                    //bottom of the page
+
+                    // setTimeout(function() { 
+                    //     $('<button>') // more of the same content button
+                    //         .attr('id', 'moreContent') // targetting
+                    //         .attr('data-searched', string) // the last thing searched
+                    //         .attr('class', 'btn btn-primary btn-block btn-sm') // button styling
+                    //         .text("find more of " + string) // text
+                    //         .appendTo('#gif-div') }, 2000)
+
         }
 
     //this onClick function will pass the buttons attrivute 'data-nameFull'
     //into our search function and produce results
-    $('#button-div').on('click', 'button', function() { // original buttons
+    $('#button-div').on('click', 'button', function() {// original buttons
         findGif( $(this).attr('data-nameFull'))
     })
 
@@ -99,18 +119,17 @@ $(document).ready(function() {
             console.log(search)
             userSearch.val("") // clear text box
         }
-    })    
-
+    })
+    
     //this click function will operate everything that happens when you
     //click on an individual picture
-    $(document.body).on('click', ('#gifContainer'), function() {
-        
+    gifDiv.on('click', 'div' ,function() {
         if ($(this).children('img').attr('data-state')=='still') { // if the image has already been clicked
             $(this).children('img')
                 .attr('src', $(this).children('img').attr('data-animate')) // reanimate
                 .attr('data-state', 'animate') // changes state
                 .removeClass('gifOnClick') // no longer see through
-            $(this).children('div').fadeOut(0) // text disappears 
+            $(this).children('.gifText').fadeOut(0) // text disappears 
  
         } else { // if the image has not been clicked already
         
@@ -119,17 +138,18 @@ $(document).ready(function() {
             .attr('src', $(this).children('img').attr('data-still')) // stops the gif
             .addClass('gifOnClick') // fadeOut animation
             .attr('data-state', 'still') // changes state
-        $(this).children('div').fadeIn() // display text (title and author)
+        $(this).children('.gifText').fadeIn() // display text (title and author)
 
         }
     })
 
     //when the user moves their mouse away or simply clicks elsewhere (mobile),
     //it executes this
-    $(document.body).on('mouseleave', ('#gifContainer'), function() {
-        $(this).children('img').attr('src', $(this).children('img').attr('data-animate')) // reanimate
-        $(this).children('img').attr('data-state', 'animate') // changes state
-        $(this).children('img').removeClass('gifOnClick') // no longer see through
-        $(this).children('div').fadeOut(0) // text disappears
+    gifDiv.on('mouseleave', 'div', function() {
+        $(this).children('img')
+            .attr('src', $(this).children('img').attr('data-animate')) // reanimate
+            .attr('data-state', 'animate') // changes state
+            .removeClass('gifOnClick') // no longer see through
+        $(this).children('.gifText').fadeOut(0) // text disappears
     })
 })
